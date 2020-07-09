69ff864603cac79baa1a4119bb55049b09f63d27
Safei Ashraf <Safei.ashraf@gmail.com>
2020-07-09 04:42:00 +0200

    fetch logic and error handling

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ let getBtn = document.querySelector('button.btn-get');
 let displayDiv = document.querySelector('div.data-display')
 let warning = document.createElement('p');
     warning.textContent =`Please Add A Valid "User Name" In Order To Get A "USER" Data!`;
-
+let warningImage = document.createElement('span');
+warningImage.className = `gif-2`;
 
 getBtn.addEventListener('click', ()=>{
     getData();
@@ -14,21 +15,54 @@ getBtn.addEventListener('click', ()=>{
 
 
 //Main Functions:
-function getData(){
+function getData()
+{
 
-    if (inputField.value = '') {
+    if (inputField.value == '') {
         displayDiv.prepend(warning);
 
     } else 
     {
-        fetch('https://api.github.com/users/safei-ashraf')
+        console.log(inputField.value);
+        fetch(`https://api.github.com/users/${inputField.value}/repos`)
         .then(res => res.json())
-        .then(data => console.log(data));
-        //Empty Display div:
-        displayDiv.innerHTML = '';
+        .then(repos => 
+                {   //Empty Display div:    
+                    displayDiv.innerHTML = '';
 
+                    console.log(repos);
+                    //Loop on repos:
+                    repos.forEach(  repo => 
+                    {
+                    //Create Main Div:
+                    let mainDiv = document.createElement('div');
+                    //Create Repo Name Text:
+                    //Append Name to main:
+                    if(repo.message){
+                        console.log(repo.message);
+                    }
+                    mainDiv.textContent = repo.name;   
+                    //Create Anchor to the repo:
+                    let theUrl = document.createElement('a');
+                    theUrl.textContent = `Visit Repo`;
+                    theUrl.href = `https://github.com/${inputField.value}/${repo.name}`;
+                    theUrl.target = `_blank`;
+                    mainDiv.appendChild(theUrl);
+                    //Add Stars info:
+                    let starsSpan = document.createElement('span');
+                    starsSpan.textContent = `Stars ${repo.stargazers_count}`;
+                    mainDiv.appendChild(starsSpan);
+                    mainDiv.className = `repo-box`;
+                    
+
+
+                    //Append to Display:
+                    displayDiv.appendChild(mainDiv);
+                    }); 
+                }
+            ).catch((error) => {
+                displayDiv.appendChild(warning);
+                displayDiv.appendChild(warningImage);
+            });
     }
-}
-function displayData(){
-
 }
